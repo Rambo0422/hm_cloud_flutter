fdc227bf004bf6aa882c834eb2f909a865b16eef
timmy_gzw <guozewen0821@gmail.com>
2023-01-09 20:24:45 +0800

    Android plugin 添加动态切换 surfaceView 和 TextureView

--- android/src/main/kotlin/com/example/hm_cloud/HmcpManagerIml.kt
+++ android/src/main/kotlin/com/example/hm_cloud/HmcpManagerIml.kt
@@ -15,11 +15,16 @@ object HmcpManagerIml {
         if (!isInit) {
             val accessKeyId = creationParams["accessKeyId"].toString()
             val channelId = creationParams["channelId"].toString()
+            val videoViewType = kotlin.runCatching {
+                creationParams["videoViewType"] as Int
+            }.getOrElse { HmcpManager.RENDER_TEXTURE_VIEW }
 
             val manager = HmcpManager.getInstance()
             val bundle = Bundle()
             bundle.putString(HmcpManager.ACCESS_KEY_ID, accessKeyId)
             bundle.putString(HmcpManager.CHANNEL_ID, channelId)
+
+            manager.videoViewType = videoViewType
             manager.init(bundle, context, object : OnInitCallBackListener {
                 override fun success() {
                     Log.e(TAG, "-------- init success --------")
